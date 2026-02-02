066689bc94285893f94f7f7fefa702295d20c9da
Jake Hamilton <jhamilton@olin.edu>
2026-02-01 22:03:12 -0500

    Add firebase config for deployment

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBT6cShwqMFiXVXJkb2HYquUqbN_Vd3dgs",
+    authDomain: "fsae-task-tracker.firebaseapp.com",
+    projectId: "fsae-task-tracker",
+    storageBucket: "fsae-task-tracker.firebasestorage.app",
+    messagingSenderId: "12476784385",
+    appId: "1:12476784385:web:1103fbbb30b2fd736dc500",
+    measurementId: "G-N9X0N4JYRP"
+};
